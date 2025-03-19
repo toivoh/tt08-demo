af7d1625c28428b4a082c563ecc1ff4139760386
Toivo Henningsson <toivo.h.h@gmail.com>
2025-03-19 17:35:54 +0100

    Try to update for IHP

--- test/tb.v
+++ test/tb.v
@@ -25,11 +25,13 @@ module tb ();
 
   tt_um_toivoh_demo user_project (
 
+/*
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
 `endif
+*/
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
